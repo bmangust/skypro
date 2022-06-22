ed49fa19727e9482a60163d7a1e22f01cd1465f3
bmangust <ftf.pollux@gmail.com>
2022-06-23 02:37:39 +0300

    add CatalogItem

---
+++ src/components/CatalogItem/CatalogItem.tsx
@@ -0,0 +1,45 @@
+import "./CatalogItem.scss";
+import { formatPrice } from "utils";
+import { ReactComponent as CartIcon } from "assets/icons/cart.svg";
+import { ReactComponent as FaveIcon } from "assets/icons/fav.svg";
+import { MouseEvent } from "react";
+
+export interface Item {
+  img: string;
+  title: string;
+  description: string;
+  price: number;
+  isFavorite?: boolean;
+}
+
+const CatalogItem = ({ img, title, description, price, isFavorite }: Item) => {
+  const handleCartClick = (e: MouseEvent) => {
+    e.stopPropagation();
+    console.log("handleCartClick");
+  };
+  const handleFaveClick = (e: MouseEvent) => {
+    e.stopPropagation();
+    console.log("handleFaveClick");
+  };
+
+  return (
+    <div className="item">
+      <div className="img">
+        <img src={img} alt={`${title}`} />
+        <div className="icon-container">
+          <button className="button icon" onClick={handleCartClick}>
+            <CartIcon />
+          </button>
+          <button className="button icon" onClick={handleFaveClick}>
+            <FaveIcon />
+          </button>
+        </div>
+      </div>
+      <span className="title">{title}</span>
+      <p className="description">{description}</p>
+      <span className="price">{formatPrice(price)} руб.</span>
+    </div>
+  );
+};
+
+export default CatalogItem;
