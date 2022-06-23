fa59e7d1d6a53b5652f5f3f34eeebc3072d370b6
bmangust <ftf.pollux@gmail.com>
2022-06-24 01:54:27 +0300

    cart state

---
+++ src/store/cart.ts
@@ -0,0 +1,43 @@
+import { makeAutoObservable } from "mobx";
+import { Item } from "./catalog";
+
+export interface CartItem {
+  item: Item;
+  quantity: number;
+}
+
+class Cart {
+  items: { [key: number]: CartItem };
+  error?: string;
+  // лимит может быть свойством Item, тогда реализация проверки чуть поменяется.
+  private limits = { min: 1, max: 10 };
+  constructor() {
+    this.items = {};
+    makeAutoObservable(this);
+  }
+
+  add = (item: Item) => {
+    if (this.items[item.id]?.quantity > this.limits.max) {
+      this.error = "Достигнуто максимальное количество";
+    } else if (this.items[item.id]) {
+      this.items[item.id].quantity += 1;
+    } else {
+      this.items[item.id] = {
+        item,
+        quantity: 1,
+      };
+    }
+  };
+  subtract = (item: Item) => {
+    if (this.items[item.id]?.quantity > 2) {
+      this.items[item.id].quantity -= 1;
+      if (this.error) delete this.error;
+    }
+  };
+
+  delete = (item: Item) => {
+    if (this.items[item.id]) delete this.items[item.id];
+  };
+}
+
+export const cart = new Cart();
